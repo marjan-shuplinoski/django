8972effa5560b712deeb6b4a120d731585c24939
marjan-shuplinoski <shuplinoskimarjan@gmail.com>
2023-02-22 07:34:43 +0100

    Added Library

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import helloWorld, zbir_na_broevi, odzemanje_na_broevi, mnozenje_na_broevi, delenje_na_broevi, \
-    mat_operacija, prv_post, mat_operacija2, proverka_godini
+    mat_operacija, prv_post, mat_operacija2, proverka_godini, PrvView, ime, ime_god, najgolem_broj,limit
 
 urlpatterns = [
     path("hello-world", helloWorld),
@@ -12,4 +12,9 @@ urlpatterns = [
     path("prv_post", prv_post),
     path("mat_operacija2", mat_operacija2),
     path("proverka_godini", proverka_godini),
+    path("prva_klasa", PrvView.as_view()),
+    path("ime",ime.as_view()),
+    path("ime_god",ime_god.as_view()),
+    path("najgolem_broj",najgolem_broj.as_view()),
+    path("limit",limit.as_view()
 ]
--- first_app/views.py
+++ first_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
 
-# CBW - Class Based Views
 
 # FBW - Function Based Views
 
@@ -122,3 +122,53 @@ def proverka_godini(request):
         return Response("{} e polnoleten".format(ime))
     else:
         return Response("{} e maloleten".format(ime))
+
+
+# CBW - Class Based Views
+
+class PrvView(APIView):
+    def get(self, request):
+        broj1 = request.GET.get('broj1', 0)
+        broj2 = request.GET.get('broj2', 0)
+        return Response({"info": "Uspesno kreirana API klasa"})
+
+    def post(self, request):
+        broj1 = request.data.get('broj1', 0)
+        broj2 = request.data.get('broj2', 0)
+        return Response({"info": {"Uspesno kreirano post metod"}})
+
+
+class ime(APIView):
+    def get(self, request):
+        ime = request.GET.get('ime', '')
+        return Response({"ime": ime, "Dolzina": len(ime)})
+
+
+class ime_god(APIView):
+    def get(self, request):
+        ime = request.GET.get('ime', '')
+        return Response({"Dobredojdovte": ime})
+
+    def post(self, request):
+        ime = request.data.get('ime', '')
+        god = int(request.data.get('god', 0))
+        if (god > 18):
+            return Response({ime: "Polnoleten"})
+        else:
+            return Response({ime: "Maloleten"})
+
+
+class najgolem_broj(APIView):
+    def post(self, request):
+        broevi = request.data.get('lista',[])
+        return Response(max(broevi))
+
+class limit(APIView):
+    def get(self, request):
+        lista = []
+        limit = int(request.GET.get('limit', 0))
+        for i in range(limit):
+            if i % 2 == 0:
+                if(i!=0):
+                    lista.append(i)
+        return Response(lista)
