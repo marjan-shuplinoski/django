b7283365d3f456fdd827c104909c46a175b9faf1
marjan <shuplinoskimarjan@gmail.com>
2023-02-28 18:04:46 +0100

    13 chas

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import Members, Books, Vehicle, Person, Transakciska_smetka , Passport
+
 
 # Register your models here.
+
+
+class MembersAdmin(admin.ModelAdmin):
+    list_display = ['ime_prezime', 'datum_zaclenuvanje', 'grad', 'godini', 'email', 'tel_broj', 'zemeno', 'plateno']
+    list_filter = ['zemeno', 'plateno']
+
+
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ['avtor', 'naslov', 'zanr', 'kolicina', 'dostapni_izdanija']
+    list_filter = ['kolicina', 'dostapni_izdanija']
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ['marka', 'model', 'godina', 'boja', 'max_brzina']
+    list_filter = ['boja', 'max_brzina']
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['ime', 'prezime', 'datum_na_ragjanje', 'embg', 'mesto_na_ziveenje', 'pol']
+    list_filter = ['mesto_na_ziveenje', 'pol']
+
+
+class TransSmetkaAdmin(admin.ModelAdmin):
+    list_display = ['br_smetka', 'datum_na_otvaranje', 'sostojba', 'klient_id']
+
+class PassportAdmin(admin.ModelAdmin):
+    list_display = ['br_passport', 'datum_na_izdavanje', 'nacionalnost', 'person_id']
+
+
+admin.site.register(Members, MembersAdmin)
+admin.site.register(Books, BooksAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Transakciska_smetka, TransSmetkaAdmin)
+admin.site.register(Passport,PassportAdmin)
+# Tipovi na relacija
+# One 2 Many
+# One 2 One
+# Many 2 Many
+
--- library/models.py
+++ library/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 # Tabeli
 
@@ -26,26 +27,59 @@ from django.db import models
 # Kolicina            int
 # Dostapni izdanija       int
 
+
+
+
+class Books(models.Model):
+    avtor = models.CharField(max_length=255)
+    naslov = models.CharField(max_length=255)
+    zanr = models.CharField(max_length=255, null=True, blank=True)
+    izdanie = models.CharField(max_length=255)
+    jazik = models.CharField(max_length=255)
+    maticna_kukja = models.CharField(max_length=255, null=True, blank=True)
+    kolicina = models.IntegerField()
+    dostapni_izdanija = models.IntegerField()
+
 class Members(models.Model):
     ime_prezime = models.CharField(max_length=255)
-    datum_zaclenuvanje = models.DateTimeField()
+    datum_zaclenuvanje = models.DateTimeField(auto_now_add=True)
     plateno = models.BooleanField()
     godini = models.IntegerField()
     adresa = models.CharField(max_length=255)
     grad = models.CharField(max_length=255)
     maticen_broj = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
-    tel_broj = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    tel_broj = models.CharField(max_length=255, null=True, blank=True)
     zemeno = models.BooleanField()
+    pozajmeno_kniga = models.ForeignKey(Books, on_delete=models.CASCADE,null=True)
 
-class Books(models.Model):
-    avtor = models.CharField(max_length=255)
-    naslov = models.CharField(max_length=255)
-    zanr = models.CharField(max_length=255)
-    izdanie = models.CharField(max_length=255)
-    jazik = models.CharField(max_length=255)
-    maticna_kukja = models.CharField(max_length=255)
-    kolicina = models.IntegerField()
-    dostapni_izdanija = models.IntegerField()
+class Vehicle(models.Model):
+    marka = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    godina = models.DateField(auto_now_add=True)
+    br_na_sedista = models.IntegerField()
+    boja = models.CharField(max_length=255)
+    max_brzina = models.IntegerField()
 
 
+class Person(models.Model):
+    ime = models.CharField(max_length=255)
+    prezime = models.CharField(max_length=255)
+    datum_na_ragjanje = models.DateField()
+    embg = models.IntegerField()
+    mesto_na_ziveenje = models.CharField(max_length=255)
+    pol = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+
+class Transakciska_smetka(models.Model):
+    br_smetka = models.IntegerField()
+    datum_na_otvaranje = models.DateField()
+    sostojba = models.IntegerField()
+    klient_id = models.ForeignKey(Person, on_delete=models.CASCADE,null=True)
+
+class Passport(models.Model):
+    br_passport = models.IntegerField()
+    datum_na_izdavanje = models.DateField()
+    izdaden_od = models.CharField(max_length=255)
+    nacionalnost = models.CharField(max_length=255)
+    person_id = models.ForeignKey(Person, on_delete=models.CASCADE,null=True)
